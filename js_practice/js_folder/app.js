28587c28053e81bafab6727a7a9f661a67704aaa
5ynn <greffincynthia@gmail.com>
2023-02-27 22:30:58 -0500

    2/27

---
+++ js_practice/js_folder/app.js
@@ -0,0 +1,41 @@
+// // console log
+
+// console.log("AYO")
+
+// // Alert
+
+// // alert("thought I was feelin you?")
+
+// // document.getElementById("demo").style.color = "blue";
+
+// document.getElementById("special").style.textDecoration = "underline";
+
+// document.getElementById("one").style.textAlign = "center";
+
+// document.getElementById("run").style.fontSize = "100px";
+
+// document.getElementById("dems").style.color = "orange";
+
+// let demo = document.getElementById("demo")
+
+// demo.innerHTML = "legz";
+
+// demo.style.color = "blue";
+
+
+// write a function to greet me
+// function declaration (load washing machine)
+// function greet() {
+//     alert("hello");
+// }
+// calling function (turn on washing machine)
+// greet()
+
+// square a number
+function square(number) {
+    return number * number;
+}
+// save value in HTML
+// let result = square(6000)
+// document.getElementById("demo").
+// innerHTML = result;
